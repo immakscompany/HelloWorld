1900cbdf155c914f4ddbd74e854ce19fffed933d
immakscompany <3359555@gmail.com>
2022-06-30 17:02:48 +0500

    Work with button configuration

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -15,18 +15,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         greetingLable.isHidden = true
-        greetingButton.layer.cornerRadius = 10
+        greetingButton.configuration = setupButton(with: "Show Greeting")
     }
 
     @IBAction func greetingButtonTapped() {
         greetingLable.isHidden.toggle()
-        greetingButton.setTitle(
-            greetingLable.isHidden ? "Show Greeting" : "Hide Greeting",
-            for: .normal
+        greetingButton.configuration = setupButton(
+            with: greetingLable.isHidden ? "Show Greeting" : "Hide Greeting"
         )
         
     }
-    
+    private func setupButton(with title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.4116370678, green: 0.2628445327, blue: 1, alpha: 1)
+        buttonConfiguration.title = title
+        buttonConfiguration.buttonSize = .large
+        buttonConfiguration.cornerStyle = .large
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 31)
+        return buttonConfiguration
+    }
 }
 
  
